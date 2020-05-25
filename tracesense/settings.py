04990b54d35dad0489b5b7fec1b62a31af22ce98
Tom <tdignan87@gmail.com>
2020-05-25 12:13:40 +0100

    added mysql to installed apps

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_mysql'
 ]
 
 MIDDLEWARE = [
@@ -118,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+"""path for static files"""
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
