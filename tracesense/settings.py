5dbeaa8d7016ff70483b7a0f15f3ad538948f456
Tom <tdignan87@gmail.com>
2020-05-25 20:29:39 +0100

    Added bootstrap forms to installed apps

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_mysql'
+    'django_mysql',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
