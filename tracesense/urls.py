9c5268a972bc3de9de34e7c48fb974d4fef318f0
Tom <tdignan87@gmail.com>
2020-05-26 11:46:09 +0100

    added url for main page

--- tracesense/urls.py
+++ tracesense/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication.views import main_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',main_page),
 ]
+ 
