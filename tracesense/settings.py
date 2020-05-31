9be552704aa0ecdea92393e6afff46489ab66867
Tom <tdignan87@gmail.com>
2020-05-31 17:32:31 +0100

    added allauth authentication backend settings

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -71,6 +71,15 @@ TEMPLATES = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    ...
+]
+
 WSGI_APPLICATION = 'tracesense.wsgi.application'
 
 
