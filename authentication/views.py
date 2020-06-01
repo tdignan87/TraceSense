a4c4765db9084c96290e45856c69bddbf59a3d53
Tom <tdignan87@gmail.com>
2020-06-01 23:10:36 +0100

    Removed unused views

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def main_page(request):
+    """ A view to return the main home page """
     return render(request,"pages/index.html")
 
 def support_page(request):
+    """ A view to return the support page """
     return render(request,"pages/support.html")
 
 def about_page(request):
+    """ A view to return the about page """
     return render(request,"pages/about.html")
