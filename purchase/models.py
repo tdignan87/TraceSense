33014825dad324bbf8d369706953500d9fc924d2
Tom <tdignan87@gmail.com>
2020-06-02 14:19:46 +0100

    created model for purchasing software

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,43 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 
 # Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=200)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=200)
+    
+    
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+    
+   
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number if it hasn't already been set.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+            
+        super().save(*args, **kwargs)
+         
+    
+
+        
