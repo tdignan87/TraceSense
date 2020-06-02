f48d179ff247bb5ffd1f2a13f1a4635008a8b00f
Tom <tdignan87@gmail.com>
2020-06-02 15:00:05 +0100

    updated with model for purchasing

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+ 
+    readonly_fields = ('order_number', 'date', 
+                       'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
