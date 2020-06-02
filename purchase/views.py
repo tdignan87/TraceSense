fb9161c213632b1d652377fa29c53ba493ab271e
Tom <tdignan87@gmail.com>
2020-06-02 20:36:45 +0100

    Added stripe public key to context

--- purchase/views.py
+++ purchase/views.py
@@ -8,6 +8,8 @@ def purchase(request):
     template = "pages/purchase.html"
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_DhfmGd6sRXHlrToC8LKDhm9G00cXBI34oU',
+        'client_secret': 'test client secret',
     }
     
     return render(request,template,context)
