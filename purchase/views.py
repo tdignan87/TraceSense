1dd02078578da5c46b5b462dd2cfc6ada21f7e53
Tom <tdignan87@gmail.com>
2020-06-02 17:55:21 +0100

    Added view for purchase orderForm model

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+def purchase(request):
+    order_form = OrderForm()
+    template = "pages/purchase.html"
+    context = {
+        'order_form': order_form,
+    }
+    
+    return render(request,template,context)
