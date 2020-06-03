c9711d7c94e02e2aabf8d1d0e46312a303a330df
Tom <tdignan87@gmail.com>
2020-06-03 18:12:42 +0100

    Added purchase confirmation URL

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from purchase.views import purchase
+from purchase.views import purchase,purchase_success
 
 urlpatterns = [
-    path('',purchase,name="purchase")
+    path('',purchase,name="purchase"),
+    path('purchase/success/<order_number>',purchase_success, name="purchase_success"),
 ]
