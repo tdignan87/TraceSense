ae03a319cceaaa73cdecd95bb9b42494c5b3efc5
Tom <tdignan87@gmail.com>
2020-06-03 14:43:07 +0100

    removed fields not required

--- purchase/admin.py
+++ purchase/admin.py
@@ -6,17 +6,14 @@ from .models import Order
 class OrderAdmin(admin.ModelAdmin):
  
     readonly_fields = ('order_number', 'date', 
-                       'order_total',
                        'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county',
-              'order_total', 'grand_total',)
+              'postcode', 'town_or_city', 'street_address', 
+              'county', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total',
                     'grand_total',)
 
     ordering = ('-date',)
