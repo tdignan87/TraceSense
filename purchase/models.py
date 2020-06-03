c6387d255cd7e79d9d903e7dff078c5e38aa3cb3
Tom <tdignan87@gmail.com>
2020-06-03 14:44:29 +0100

    removed order total as only grand is required

--- purchase/models.py
+++ purchase/models.py
@@ -16,8 +16,7 @@ class Order(models.Model):
     street_address = models.CharField(max_length=80, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=200)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=200)
+    grand_total = models.IntegerField( null=False, default=200)
     
     
     def _generate_order_number(self):
