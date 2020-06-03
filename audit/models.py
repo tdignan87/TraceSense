ed2d4deda0d45c52df7d2652c30ba1e45987f099
Tom <tdignan87@gmail.com>
2020-06-03 01:42:23 +0100

    Created models for audit tables

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class gmp_questions(models.Model):
+    
+     gmp_questionid = models.AutoField(primary_key=True)
+     question = models.CharField(max_length=255, null=False, editable=True)
+     created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
+     created_by = models.CharField(max_length=30, null=True, editable=True)
+     
+class area(models.Model):
+    
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=5,null=False,editable=True)
+    name = models.CharField(max_length=255,null=False,editable=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
+    created_by = models.CharField(max_length=30, null=False, editable=True)
+    
+    
+class department(models.Model):
+    
+    department_id = models.AutoField(primary_key=True)
+    department = models.CharField(max_length=50,null=False,editable=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
+    created_by = models.CharField(max_length=30, null=False, editable=True)
