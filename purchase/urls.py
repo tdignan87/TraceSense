d983618d8a7ae434d4275e2e95cb6926e9ed19ce
Tom <tdignan87@gmail.com>
2020-06-03 19:23:04 +0100

    added webhooks

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from purchase.views import purchase,purchase_success
+from .webhooks import webhook
 
 urlpatterns = [
     path('',purchase,name="purchase"),
     path('purchase/success/<order_number>',purchase_success, name="purchase_success"),
+    path('wh/', webhook, name="webhook"),
 ]
