ce0d8d8a87c8796e84478e5b767176a47fca2f68
Tom <tdignan87@gmail.com>
2020-06-03 18:14:39 +0100

    added POST function for adding data into DB and updating

--- purchase/views.py
+++ purchase/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from purchase.models import Order
 
 import stripe
 
@@ -10,7 +11,27 @@ def purchase(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     
-    total_price = 200
+    if request.method == 'POST':
+        
+        form_data = {
+            
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address': request.POST['street_address'],
+           
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+           order = order_form.save()
+           return redirect(reverse('purchase_success',args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. Please double check your information and try again.')
+    else:
+        total_price = 200
     stripe_total = total_price
     stripe.api_key = stripe_secret_key
     stripe.PaymentIntent.create(
@@ -31,3 +52,20 @@ def purchase(request):
     }
     
     return render(request,template,context)
+
+def purchase_success(request, order_number):
+    """
+    Handle successful payments
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order,order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+            email will be sent to {order.email}.')
+    
+    template = "pages/success.html"
+    context = {
+        'order': order,
+    }
+    
+    return render(request, template, context)
