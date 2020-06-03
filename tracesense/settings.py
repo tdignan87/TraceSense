8b20ec76106c863250fec6ecbc6b2fd5863af467
Tom <tdignan87@gmail.com>
2020-06-03 22:12:22 +0100

    moved media files to media folder

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -175,7 +175,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
@@ -189,9 +190,12 @@ if 'USE_AWS' in os.environ:
     
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
     
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
