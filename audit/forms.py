95a158c0424284883d2a782f08a59caf925872f9
Tom <tdignan87@gmail.com>
2020-06-03 13:41:31 +0100

    no changes

--- audit/forms.py
+++ audit/forms.py
@@ -4,4 +4,4 @@ from .models import gmp_question
 class GMPQuestions(forms.ModelForm):
     class Meta:
         model = gmp_question
-        fields = ('question','deleted',)
+        fields = ('question','deleted',) 
