28a11711d3114e1faaf45279ee9aeeec298e1cc0
Tom <tdignan87@gmail.com>
2020-06-03 13:44:11 +0100

    removed console log for testing

--- purchase/static/js/stripe_elements.js
+++ purchase/static/js/stripe_elements.js
@@ -2,6 +2,7 @@ var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var stripe_public_key = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
+
 var style = {
     base: {
         color: '#000',
@@ -19,7 +20,3 @@ var style = {
 };
 var card = elements.create('card', { style: style });
 card.mount('#card-element');
-
-$(document).ready(function() {
-    console.log("ready!");
-});
