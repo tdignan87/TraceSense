8aa41477db282085545234a8da771bfe9d2d2814
Tom <tdignan87@gmail.com>
2020-06-03 21:44:26 +0100

    updated region name

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'tracesense-staticfiles-user'
-    AWS_S3_REGION_NAME = 'EU (London)'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
