34c4e15a44c62bb5a1276d3e915b09ddfa3cdf4a
Tom <tdignan87@gmail.com>
2020-06-03 15:35:31 +0100

    added env variables for stripe settings, set total price

--- purchase/views.py
+++ purchase/views.py
@@ -7,16 +7,27 @@ from .forms import OrderForm
 import stripe
 
 def purchase(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     
-    total_price = OrderForm
-    total = total_price['grand_total']
-    stripe_total = round(total * 100)
+    total_price = 200
+    stripe_total = total_price
+    stripe.api_key = stripe_secret_key
+    stripe.PaymentIntent.create(
+       amount=stripe_total,
+       currency=settings.STRIPE_CURRENCY, 
+    )
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it?')
+    
+    
     order_form = OrderForm()
     template = "pages/purchase.html"
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_DhfmGd6sRXHlrToC8LKDhm9G00cXBI34oU',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': stripe_secret_key,
     }
     
     return render(request,template,context)
