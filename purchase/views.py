d34c731e71513757d83dec88c3f4d7f89646ae07
Tom <tdignan87@gmail.com>
2020-06-04 15:22:12 +0100

    Added Post for checkout data

--- purchase/views.py
+++ purchase/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -6,6 +7,21 @@ from .forms import OrderForm
 from purchase.models import Order
 
 import stripe
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid,metadata={
+            'save_info':request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def purchase(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
