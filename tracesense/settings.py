842094e81e3cedfebd5c64d233d15d13494fc1ad
Tom <tdignan87@gmail.com>
2020-06-04 15:19:58 +0100

    Use aws comment is set to false for now

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'audit',
     'purchase',
+    'profile',
+    'tracesense',
     
    
    # Other
@@ -183,7 +185,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
+# set to disabled in Heroku for now as using whitenoise
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'tracesense'
