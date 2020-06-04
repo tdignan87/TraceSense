3069873a45f4d068eeb2b6084d0085630d5589a7
Tom <tdignan87@gmail.com>
2020-06-04 11:55:27 +0100

    added stripe handler for payment succeed and failed

--- purchase/webhooks.py
+++ purchase/webhooks.py
@@ -34,7 +34,25 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
+    
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+    
+    # Map webhook events to relevant handler functions
+    
+    event_map = {
+        'payment_intend.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intend.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
 
-
+    # Get the webhook type from Stripe
+    
+    event_type = event['type']
+    
+    event_handler = event.map.get(event_type, handler.handle_event)
+    
+    #Call the event handler with the event
+    response = event_handler(event)
+    return response
     
     
