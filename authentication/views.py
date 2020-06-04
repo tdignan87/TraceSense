eeda852d36264c69671b331009423dccaafdab71
Tom <tdignan87@gmail.com>
2020-06-04 15:23:21 +0100

    no changes

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
+
 def main_page(request):
     """ A view to return the main home page """
     return render(request,"pages/index.html")
-
 def support_page(request):
     """ A view to return the support page """
     return render(request,"pages/support.html")
