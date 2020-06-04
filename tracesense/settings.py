860ff3a7718d8c9a9270ad408e571e529a1613fd
Tom <tdignan87@gmail.com>
2020-06-04 12:13:05 +0100

    updated static file dirs

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "tracesense/staticfiles")
+    os.path.join(BASE_DIR, "purchase/static")
                  ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
