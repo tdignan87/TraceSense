f6447f5d04435dea7ec6d14fcbb7a61b4b586eb5
Tom <tdignan87@gmail.com>
2020-06-06 22:35:31 +0100

    removed as no longer used

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
+from allauth.account.adapter import Default
 
 
-def main_page(request):
-    """ A view to return the main home page """
-    return render(request,"pages/index.html")
-def support_page(request):
-    """ A view to return the support page """
-    return render(request,"pages/support.html")
-
-def about_page(request):
-    """ A view to return the about page """
-    return render(request,"pages/about.html")
