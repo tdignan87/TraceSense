3cf0cd0cdd1e8bd0d2eab4ed7f7b34cbcb6e1287
Tom <tdignan87@gmail.com>
2020-06-08 23:11:07 +0100

    changed db back to postgres for push to heroku

--- tracesense/views.py
+++ tracesense/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import user_logged_in
 
 
 def main_page(request):
     """ A view to return the main home page """
     return render(request,"pages/index.html")
+
 def support_page(request):
     """ A view to return the support page """
     return render(request,"pages/support.html")
 
+
 def about_page(request):
     """ A view to return the about page """
     return render(request,"pages/about.html")
+
