61d790a1b2068da5b2d0d0f8514ce7bcd53cbe68
Tom <tdignan87@gmail.com>
2020-06-08 23:06:24 +0100

    created model for departments, changed to Foreign key for userID

--- audit/models.py
+++ audit/models.py
@@ -1,15 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class gmp_questions(models.Model):
     
+     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
      gmp_questionid = models.AutoField(primary_key=True)
      question = models.CharField(max_length=255, null=False, editable=True)
      created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
-     created_by = models.CharField(max_length=30, null=True, editable=True)
-     
-    
      
 class area(models.Model):
     
@@ -17,17 +16,17 @@ class area(models.Model):
     code = models.CharField(max_length=5,null=False,editable=True)
     name = models.CharField(max_length=255,null=False,editable=True)
     created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
-    created_by = models.CharField(max_length=30, null=False, editable=True)
     
     
 class department(models.Model):
-    
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     department_id = models.AutoField(primary_key=True)
     department = models.CharField(max_length=50,null=False,editable=True)
     created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
-    created_by = models.CharField(max_length=30, null=False, editable=True)
+    
     
 class gmp_answers(models.Model):
+    
     gmp_id = models.AutoField(primary_key=True)
     gmp_questionid = gmp_questions.gmp_questionid
     compliant = models.SmallIntegerField(null=True,editable=True)
