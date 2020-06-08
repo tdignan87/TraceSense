c958a9bd8fa3cf9bb52b59e12bc332bed5ee94c2
Tom <tdignan87@gmail.com>
2020-06-08 23:14:05 +0100

    created new view for departments

--- audit/views.py
+++ audit/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import NewDepartment
+from .models import department
+
 
 def new_department(request):
     """ A view to return to the main home page """
-    return render(request,"pages/new_department.html")
+    form = NewDepartment()
+    if request.method == "POST":
+        form = NewDepartment(request.POST)
+    if form.is_valid():
+         new_department = form.save(commit=False)
+         new_department.user_id = request.user.id
+         new_department.save()
+         return redirect("/profile/")
+    context = {'form':form}
+    return render(request,"pages/new_department.html",context)
+    
+    
+
+
+def department_list(request):
+    
+    departments = department.objects.filter(user_id=request.user.id)
+    return render(request,"pages/view_departments.html", {"departments":departments})
+
+def update_department(request, pk):
+    form = NewDepartment()
+    context = {'form':form}
+    return render(request,"pages/new_department.html",context)
