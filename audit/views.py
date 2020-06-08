946c266607f84ea6b7292f43e14c518e1dd50167
Tom <tdignan87@gmail.com>
2020-06-08 12:38:38 +0100

    added view for new department page

--- audit/views.py
+++ audit/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-def config(request):
-    """ A view to return the main home page """
-    return render(request,"pages/config.html")
-
+def new_department(request):
+    """ A view to return to the main home page """
+    return render(request,"pages/new_department.html")
