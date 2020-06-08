f5a65185e2d7c5519167ed44155587fc560e78c4
Tom <tdignan87@gmail.com>
2020-06-08 12:51:18 +0100

    changed url path and removed extra spacing

--- audit/urls.py
+++ audit/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from audit.views import new_department
 
 urlpatterns = [
-    path('new_department/',new_department,name="new_department"),
+    path('new/',new_department,name="new_department"),
     
 ]
 
