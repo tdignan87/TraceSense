4f287802a4a08242a0309e8dc7e42b946056e43c
Tom <tdignan87@gmail.com>
2020-06-08 23:13:41 +0100

    added URLS for updating and editing departments

--- audit/urls.py
+++ audit/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from audit.views import new_department
+from audit.views import new_department,update_department,department_list
 
 urlpatterns = [
     path('new/',new_department,name="new_department"),
-    
+    path('update/<str:pk>',update_department,name="update_department"),
+    path('edit/',department_list,name="department_list"),
 ]
