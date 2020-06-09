67e42275e309dd42baeab9e121f1d42004b04f96
Tom <tdignan87@gmail.com>
2020-06-09 19:44:00 +0100

    changed db to postgres

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'tracesense.wsgi.application'
  #Database
  #https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
-     DATABASES = {
+    DATABASES = {
          'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
      }
 else:
