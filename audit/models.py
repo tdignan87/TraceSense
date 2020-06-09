9b72f0d6a2b83278adac5ce48810c76c6d54c0e8
Tom <tdignan87@gmail.com>
2020-06-09 19:45:44 +0100

    added userID foreign key to area model

--- audit/models.py
+++ audit/models.py
@@ -10,9 +10,9 @@ class gmp_questions(models.Model):
      question = models.CharField(max_length=255, null=False, editable=True)
      created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
      
-class area(models.Model):
+class areas(models.Model):
     
-    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     code = models.CharField(max_length=5,null=False,editable=True)
     name = models.CharField(max_length=255,null=False,editable=True)
     created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
