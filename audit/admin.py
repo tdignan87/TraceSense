314a5b90dae269965e4ceaedfef1ce09c77c755c
Tom <tdignan87@gmail.com>
2020-06-10 20:08:32 +0100

    removed as now in own proper app

--- audit/admin.py
+++ audit/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from .models import gmp_questions
-
-class GMPQuestionAdmin(admin.ModelAdmin):
-    
-    readonly_fields = ('gmp_questionid','created','user_id',)
-    
-    fields = ('question',)
-    
-    list_display = ('gmp_questionid', 'created', 'question')
-    
-    
-admin.site.register(gmp_questions, GMPQuestionAdmin)
