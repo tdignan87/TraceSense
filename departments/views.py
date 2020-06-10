20317291be2b1784edaefc2462dc8518410a7f69
Tom <tdignan87@gmail.com>
2020-06-10 01:57:07 +0100

    created view for creating new department

--- departments/views.py
+++ departments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import NewDepartment
+from .models import department
 
-# Create your views here.
+def create_department(request):
+    """create new department into the database."""
+    new_department = NewDepartment()
+    if request.method == "POST":
+        form = NewDepartment(request.POST)
+        if form.is_valid():
+            createDepartment = form.save(commit=False)
+            createDepartment.user_id = request.user.id
+            form.save()
+            return redirect("/profile")
+    context = {'new_department':NewDepartment}
+    return render(request,"pages/new_department.html",context)
