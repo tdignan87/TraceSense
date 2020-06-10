540c717aa0cdde03093616cf22a994a253a80263
Tom <tdignan87@gmail.com>
2020-06-10 01:59:53 +0100

    removed department class

--- audit/models.py
+++ audit/models.py
@@ -26,13 +26,6 @@ class areas(models.Model):
         return self.name
     
     
-class department(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    department_id = models.AutoField(primary_key=True)
-    department = models.CharField(max_length=50,null=False,editable=True)
-    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
-    
-    
 class gmp_answers(models.Model):
     
     gmp_id = models.AutoField(primary_key=True)
