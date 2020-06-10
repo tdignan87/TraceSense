8d47ae7af14b2833fbf6f9ae671e2ad5a92f47fb
Tom <tdignan87@gmail.com>
2020-06-10 16:28:57 +0100

    removed as placed into own URLs in own app

--- audit/urls.py
+++ audit/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from audit.views import create_area,area_list,update_area,delete_area
+
 
 urlpatterns = [
-    path("area/",create_area,name="create_area"),
-    path("area_list/",area_list,name="area_list"),
-    path("update_area/<str:pk>",update_area,name="update_area"),
-    path("delete_area/<str:pk>",delete_area,name="delete_area"),
+  
 ]
