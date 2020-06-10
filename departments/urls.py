74dc4f1601f19cc3437ab78ec2fe2b427094a6e3
Tom <tdignan87@gmail.com>
2020-06-10 17:57:30 +0100

    added delete department views to url patern

--- departments/urls.py
+++ departments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from departments.views import create_department,department_list,update_department
+from departments.views import create_department,department_list,update_department,delete_department
 
 urlpatterns = [
   path("departments/",create_department,name="create_department"),
   path("view_departments/",department_list,name="list_departments"),
   path("update_department/<str:pk>",update_department,name="update_department"),
-  
+  path("delete_department/<str:pk>",delete_department,name="delete_department"),
 ]
