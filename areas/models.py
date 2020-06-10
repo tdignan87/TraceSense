3803a7c21af998bb5ed9769799ede24f46f57b48
Tom <tdignan87@gmail.com>
2020-06-10 16:32:45 +0100

    initial commit

--- areas/models.py
+++ areas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Areas(models.Model):
+    
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    code = models.CharField(max_length=5,null=False,editable=True)
+    name = models.CharField(max_length=255,null=False,editable=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
+    
+    def __str__(self):
+        return self.name
