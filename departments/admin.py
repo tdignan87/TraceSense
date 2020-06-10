cd0ff4d6d2f7e0dbd09167d93340686a7ac3da43
Tom <tdignan87@gmail.com>
2020-06-10 01:57:50 +0100

    created departments admin page

--- departments/admin.py
+++ departments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import department
 
 # Register your models here.
+
+class Departments(admin.ModelAdmin):
+    
+    readonly_fields = ('department_id','created','user')
+    
+    fields = ('department',)
+    
+    list_display = ('department_id', 'created','department',)
+    
+admin.site.register(department,Departments)
