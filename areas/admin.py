7775f2e70c482034221c1bac8c1faee1218000be
Tom <tdignan87@gmail.com>
2020-06-10 16:25:11 +0100

    created admin model

--- areas/admin.py
+++ areas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Areas
 
-# Register your models here.
+class SiteAreas(admin.ModelAdmin):
+    
+    readonly_fields = ('id','created',)
+    
+    fields = ('code','name','created_by',)
+    
+    list_display = ('id', 'code','name',)
+
+admin.site.register(Areas, SiteAreas)
