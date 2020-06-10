eaed1711430fc5fb915777e11b8192117bc267a8
Tom <tdignan87@gmail.com>
2020-06-10 20:08:52 +0100

    removed as now in own app

--- audit/forms.py
+++ audit/forms.py
@@ -1,11 +1,1 @@
 from django import forms
-from .models import gmp_questions
-
-class GMPQuestions(forms.ModelForm):
-    class Meta:
-        model = gmp_questions
-        fields = ('question',) 
-        
-            
-
-        
