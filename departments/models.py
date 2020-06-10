44fffb0659a15361d98a583b10192df89a2449e2
Tom <tdignan87@gmail.com>
2020-06-10 01:57:28 +0100

    created model for adding new department

--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class department(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    department_id = models.AutoField(primary_key=True)
+    department = models.CharField(max_length=50,null=False,editable=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
