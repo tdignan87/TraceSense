6d4202570b1c570421d39ea603fb30da37ead830
Tom <tdignan87@gmail.com>
2020-06-11 19:08:48 +0100

    Created view for rendering audit page

--- audit/views.py
+++ audit/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render,redirect
+from .models import Transactions
+
+
+def new_audit(request):
+    """ A view to return the page for creating a new audit. """
+    return render(request,"pages/new_audit.html")
