0fbe771355fe39c4e4abf9c52c06a2b24ea54566
Tom <tdignan87@gmail.com>
2020-06-11 02:25:18 +0100

    Added URL for viewing questions

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from questions.views import create_question
+from questions.views import create_question,question_list,update_question,delete_question
 
 
 urlpatterns = [
-  path("questions/",create_question,name="create_question") 
+  path("questions/",create_question,name="create_question"),
+  path("question_list/",question_list,name="view_questions"),
+  path("question_update/<str:pk>",update_question,name="update_question"),
+  path("delete_question/<str:pk>",delete_question,name="delete_question"),
 ]
