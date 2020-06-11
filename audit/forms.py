41bac6742ab30d19e0fe3a5a441f06a2621318ba
Tom <tdignan87@gmail.com>
2020-06-11 19:09:37 +0100

    Created form for audit

--- audit/forms.py
+++ audit/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from .models import Transactions
+
+class AuditTransaction(forms.ModelForm):
+    class Meta:
+        model = Transactions
+        
+        readonly_fields = ('created',)
+        
+        fields = ('compliant','fail','freetext','status')
+        
+        
+        
