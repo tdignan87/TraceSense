5b91830d3e00f8d1ff7700434d92f3e35e7a2602
Tom <tdignan87@gmail.com>
2020-06-12 23:19:06 +0100

    created URL for send page

--- send/urls.py
+++ send/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import send_form
 
 urlpatterns = [
-    path('', views.send_form)
+     path("send/",send_form,name="new_enquiry"),
+     path("confirmation/",send_form,name="confirmation_sent")
 ]
