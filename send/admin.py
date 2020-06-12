7227a46b9ac8528287658e676579d146eb5b7652
Tom <tdignan87@gmail.com>
2020-06-12 23:19:32 +0100

    created admin form for sending mail

--- send/admin.py
+++ send/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+class ContactFormAdmin(admin.ModelAdmin):
+    
+    readonly_fields = ('contact_id','created',)
+    
+    fields = ('contact_name','contact_email','enquiry_details',)
+    
+    list_display = ('contact_name', 'contact_email','enquiry_details',)
+
+admin.site.register(ContactForm, ContactFormAdmin)
