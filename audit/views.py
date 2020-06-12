cd7ebed26b6afd5645d4f2ec9c31c03981d1e17c
Tom <tdignan87@gmail.com>
2020-06-12 11:56:18 +0100

    changed coments for new audit function

--- audit/views.py
+++ audit/views.py
@@ -3,5 +3,5 @@ from .models import Transactions
 
 
 def new_audit(request):
-    """ A view to return the page for creating a new audit. """
+    """ Create new audit and store into the database"""
     return render(request,"pages/new_audit.html")
