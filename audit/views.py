8af455d49751ab8d41437fe748fcf3881296834a
Tom <tdignan87@gmail.com>
2020-06-12 13:36:48 +0100

    changed placeholder text

--- audit/views.py
+++ audit/views.py
@@ -3,5 +3,5 @@ from .models import Transactions
 
 
 def new_audit(request):
-    """ Create new audit and store into the database"""
+    """ Create new audit and store into the database. This is just a test"""
     return render(request,"pages/new_audit.html")
