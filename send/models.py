4f89504a870b0876be51a676e3388e04d28aaf32
Tom <tdignan87@gmail.com>
2020-06-12 23:19:18 +0100

    created model for sending mail

--- send/models.py
+++ send/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ContactForm(models.Model):
+    
+    contact_id = models.AutoField(primary_key=True)
+    contact_name = models.CharField(max_length=55,null=False,editable=True)
+    contact_email = models.EmailField(max_length=55,null=False,editable=True)
+    enquiry_details = models.CharField(max_length=255,null=False,editable=True)
+    created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
+    
