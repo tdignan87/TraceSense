dfffbc6c4cdf1620ae4b0a3e2fed86105bacfe91
Tom <tdignan87@gmail.com>
2020-06-14 10:57:12 +0100

    Added mail send to confirm mail function

--- send/views.py
+++ send/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from .forms import ContactUsForm
+from tracesense.settings import EMAIL_HOST_USER
 
 def send_form(request):
     """ Form for sending email enquiry into tracesense. Exception if SMTP fails """
@@ -8,33 +9,30 @@ def send_form(request):
     form_class = ContactUsForm
     if request.method == "POST":
         form = ContactUsForm(request.POST)
-        if sendform.is_valid():
+        if form.is_valid():
             form.save()
-            try:
-                send_data()
-                return render(request,"pages/confirmation.html")
-            except:
-                render(request,"pages/error-form.html")
-        return render(request,"pages/confirmation.html")
-            
+            return redirect(confirm_mail)
     context = {'form':ContactUsForm}
     return render(request,"pages/contact-form.html",context)
 
 def confirm_mail(request):
-    return render(request,"pages/confirmation.html")
-
-def send_data(request):
-    form = ContactUsForm()
-    contact_name = form.cleaned_data['contact_name']
-    contact_email = form.cleaned_data['contact_email']
-    enquiry_details = form.cleaned_data['enquiry_details']
-    send_mail()
-    
-    send_mail (
+    """ try and send email - if fails return error page"""
+    try:
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            contact_name = form.cleaned_data['contact_name']
+            contact_email = form.cleaned_data['contact_email']
+            contact_message = form.cleaned_data['contact_message']
+           
+        send_mail(
         'Contact Form Enquiry',
-        enquiry_details,
-        contact_email,
+        'Test Email',
+        'tracesense@tracesense.info',
         ['tom.dignan@tracesense.co.uk'],
-         fail_silently=False,
+        fail_silently=False,
         )
-  
+        return render(request,"pages/confirmation.html")
+    except:
+        return render(request,"pages/error-form.html")
+
+
