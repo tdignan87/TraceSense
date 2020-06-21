636d890185592b1e4d34daf36f9827668aa3c35d
Tom <tdignan87@gmail.com>
2020-06-21 22:47:24 +0100

    changed to live db

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'tracesense.wsgi.application'
  #https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
      }
 else:
     DATABASES = {
