f6bc5d49be74d09805c9933e7e74b02fd206f6ff
Tom <tdignan87@gmail.com>
2020-06-21 22:51:27 +0100

    removed comments and added verbose name

--- areas/models.py
+++ areas/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Locations(models.Model):
+    class Meta:
+            verbose_name_plural = "locations"
     
     location_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
