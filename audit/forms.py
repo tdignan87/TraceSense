5c4b2750dbdb7812f00a0e9170054a65ef8ab1b2
Tom <tdignan87@gmail.com>
2020-06-21 22:51:56 +0100

    created form for audit form

--- audit/forms.py
+++ audit/forms.py
@@ -7,7 +7,12 @@ class AuditTransaction(forms.ModelForm):
         
         readonly_fields = ('created',)
         
-        fields = ('compliant','fail','freetext','status')
+        fields = ('compliant','freetext','status')
+       
+        compliant_choices = [1,2]
         
-        
-        
+        widgets = {
+            'status': forms.Select(attrs={"class": "form-control required"}),
+            'freetext': forms.TextInput(attrs={"class": "form-control"}),
+            'body': forms.TextInput(attrs={"class": "form-control"})      }
+      
