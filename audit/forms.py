6293c867a605ddddc60c7909aa5add2d9bfcf297
Tom <tdignan87@gmail.com>
2020-06-23 12:53:50 +0100

    added gmp question into fields

--- audit/forms.py
+++ audit/forms.py
@@ -7,12 +7,18 @@ class AuditTransaction(forms.ModelForm):
         
         readonly_fields = ('created',)
         
-        fields = ('compliant','freetext','status')
+        fields = ('gmp_question','compliant','freetext','status','location')
        
         compliant_choices = [1,2]
         
         widgets = {
+            
+            'gmp_question': forms.Select(attrs={"class": "form-control required"}),
             'status': forms.Select(attrs={"class": "form-control required"}),
             'freetext': forms.TextInput(attrs={"class": "form-control"}),
-            'body': forms.TextInput(attrs={"class": "form-control"})      }
+            'body': forms.TextInput(attrs={"class": "form-control"}),
+            'location':forms.Select(attrs={"class": "form-control required"}),        
+            
+            
+            }
       
