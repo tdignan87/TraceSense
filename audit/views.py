3472fbc571cb80d34f2add4d308ee50b80838466
Tom <tdignan87@gmail.com>
2020-06-24 16:54:28 +0100

    added get gmp question data

--- audit/views.py
+++ audit/views.py
@@ -9,11 +9,13 @@ def new_audit(request):
     auditForm = AuditTransaction()
     my_questions = Gmp_questions.objects.filter(user=request.user)
     my_locations = Locations.objects.filter(user=request.user)
+    gmp_data_audit = Transactions.objects.filter(user=request.user)
     if request.method == "POST":
         form = AuditTransaction(request.POST)
         if form.is_valid():
-            createAudit = auditForm.save(commit=False)
+            createAudit = form.save(commit=False)
             createAudit.user_id = request.user.id
+            my_questions = form.cleaned_data.get('gmp_question')
             form.save()
             return render(request,"pages/view_audits.html")
     context = {'form':auditForm,
