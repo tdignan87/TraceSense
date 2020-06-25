54c5d0d4be6df1742b2cfd1838961083485a5d9d
Tom <tdignan87@gmail.com>
2020-06-25 16:41:41 +0100

    changed DB to postgres for Heroku git push

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY_MAIN")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
    # Other
    'crispy_forms',
    'storages',
+   
 ]
 
 MIDDLEWARE = [
