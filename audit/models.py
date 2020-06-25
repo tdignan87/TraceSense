89296ca74a9e120d8cb1c580a2db6c82aac42d4a
Tom <tdignan87@gmail.com>
2020-06-25 16:46:03 +0100

    Changed models to CASCADE instead of delete

--- audit/models.py
+++ audit/models.py
@@ -9,15 +9,16 @@ class Transactions(models.Model):
     
     audit_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
-    location = models.ForeignKey(Locations,null=False,on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(Locations,null=False,on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, null=False, editable=False)
-    gmp_question = models.ForeignKey(Gmp_questions,null=False,on_delete=models.DO_NOTHING,related_name='gmp_questions')
+    gmp_question = models.ForeignKey(Gmp_questions,null=False,on_delete=models.CASCADE,related_name='gmp_questions')
     compliant = models.BooleanField("Is Compliant?/No Risk",default=False)
-    freetext = models.CharField(max_length=255,null=True,editable=True)
+    freetext = models.CharField(max_length=255,null=True,editable=True,blank=True)
     
     statusChoice =(('Open','Open'),
         ('Closed','Closed'))
     
     status = models.CharField(max_length=6,choices=statusChoice,null=False,editable=True)
     
-    
+    def __str__(self):
+        return self.gmp_question.question
