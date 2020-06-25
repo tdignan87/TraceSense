f9cdaa4b0613ff99ed690886f448235b3f4913ca
Tom <tdignan87@gmail.com>
2020-06-25 16:43:57 +0100

    moved location to top of field

--- audit/forms.py
+++ audit/forms.py
@@ -7,18 +7,17 @@ class AuditTransaction(forms.ModelForm):
         
         readonly_fields = ('created',)
         
-        fields = ('compliant','freetext','status','location')
+        fields = ('location','gmp_question','compliant','freetext','status')
        
         compliant_choices = [1,2]
         
         widgets = {
             
-           # 'gmp_question': forms.Select(attrs={"class": "form-control required"}),
+            'location':forms.Select(attrs={"class": "form-control required"}), 
+            'gmp_question': forms.Select(attrs={"class": "form-control required"}),
             'status': forms.Select(attrs={"class": "form-control required"}),
             'freetext': forms.TextInput(attrs={"class": "form-control"}),
-            'body': forms.TextInput(attrs={"class": "form-control"}),
-            'location':forms.Select(attrs={"class": "form-control required"}),        
-            
-            
+            'body': forms.TextInput(attrs={"class": "form-control"}),      
+        
             }
       
--- audit/urls.py
+++ audit/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from audit.views import new_audit
+from audit.views import new_audit, audit_list,update_audit,delete_audit,open_actions
 
 urlpatterns = [
   path("questions/",new_audit,name="new_audit"),
- 
+  path("view_audits/",audit_list,name="list_audits"),
+  path("edit_audits/<str:pk>",update_audit,name="update_audits"),
+  path("delete_audits/<str:pk>",delete_audit,name="delete_audit"),
+  path("open_actions/",open_actions,name="open_actions"),
 ]
