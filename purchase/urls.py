086c750c3223e1e6cd20e83b7600ae1813d8b0d8
Tom <tdignan87@gmail.com>
2020-07-01 13:28:25 +0100

    added URL for payment failure page

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from purchase.views import purchase,purchase_success, cache_checkout_data
+from purchase.views import purchase,purchase_success, cache_checkout_data,purchase_failure
 from .webhooks import webhook
 
 urlpatterns = [
     path('',purchase,name="purchase"),
     path('purchase/success/<order_number>',purchase_success, name="purchase_success"),
+    path('purchase/failure/',purchase_failure, name="purchase_failure"),
     path('cache_checkout_data/',cache_checkout_data, name="cache_checkout_data"),
     path('wh/', webhook, name="webhook"),
 ]
