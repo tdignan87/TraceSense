232153e5147c3150f85b1ec48e2bcb41828cac9b
Tom <tdignan87@gmail.com>
2020-07-02 22:44:39 +0100

    changed URL for submit btn

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -61,7 +61,7 @@ form.addEventListener('submit', function(ev) {
         'client_secret': clientSecret,
         'save_info': saveInfo,
     };
-    var url = '/checkout/cache_checkout_data/';
+    var url = '/purchase/success/';
 
     $.post(url, postData).done(function() {
         stripe.confirmCardPayment(clientSecret, {
