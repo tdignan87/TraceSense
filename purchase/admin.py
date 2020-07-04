a6ba0903a12e99c9c78c31995662ee17a34e1c43
Tom <tdignan87@gmail.com>
2020-07-04 21:00:43 +0100

    removed

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,21 +1,2 @@
 from django.contrib import admin
 from .models import Order
-# Register your models here.
-
-
-class OrderAdmin(admin.ModelAdmin):
- 
-    readonly_fields = ('order_number', 'date', 
-                       'grand_total',)
-
-    fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address'
-              , 'grand_total',)
-
-    list_display = ('order_number', 'date', 'full_name',
-                    'grand_total',)
-
-    ordering = ('-date',)
-
-admin.site.register(Order, OrderAdmin)
