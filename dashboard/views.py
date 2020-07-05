a63020a9aa658c78745296e5a2316110a5f61877
Tom <tdignan87@gmail.com>
2020-07-05 17:17:16 +0100

    Removed unnecessary code

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from checkout.models import Order
+
 
 
 
 def profile_page(request):
    """ renders dashboard page showing information and menu items to user."""
-   has_paid = Order.objects.filter(user_id=request.user)
   
-   context = {'order':has_paid,}
-
-   return render(request,"dashboard.html",context)
+   return render(request,"dashboard.html")
 
 def change_password(request):
     return render(request,"allauth/account/password_change.html")
