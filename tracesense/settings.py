1a32a6c65a1b41199733b75d93de7c885bb1b3ed
Tom <tdignan87@gmail.com>
2020-07-05 22:24:07 +0100

    changed to postgres DB

--- tracesense/settings.py
+++ tracesense/settings.py
@@ -135,7 +135,7 @@ if 'DATABASE_URL' in os.environ:
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 else:
-    DATABASES = {
+ DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.mysql',
            'OPTIONS': {
