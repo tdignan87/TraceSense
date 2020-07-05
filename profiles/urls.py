384e757f9a9c8943adcd85630796b5eb5e842aea
Tom <tdignan87@gmail.com>
2020-07-05 19:25:10 +0100

    no changes

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,7 +3,7 @@ from profiles.views import change_password,reset_password
 from dashboard.views import profile_page
 
 urlpatterns = [
-    path("",profile_page,name="dashboard"),
+    path("",profile_page,name="profile_page"),
     path("password/change/",change_password,name="change_password"),
     path("password/reset/",reset_password,name="reset_password"),
 ]
