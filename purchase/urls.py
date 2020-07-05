f6028a71e6f6de176514c4bfefcb4fa842a22362
Tom <tdignan87@gmail.com>
2020-07-05 13:48:22 +0100

    removed

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from purchase.views import purchase,purchase_success, cache_checkout_data,purchase_failure
-from .webhooks import webhook
+
 
 urlpatterns = [
-    path('',purchase,name="purchase"),
-    path('purchase/success/<order_number>',purchase_success, name="purchase_success"),
-    path('purchase/failure/',purchase_failure, name="purchase_failure"),
-    path('cache_checkout_data/',cache_checkout_data, name="cache_checkout_data"),
-    path('wh/', webhook, name="webhook"),
+   
 ]
