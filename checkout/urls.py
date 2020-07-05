1b1c4f5d7f293a5068cb7981b1fd3631d1ab3cb0
Tom <tdignan87@gmail.com>
2020-07-05 13:49:45 +0100

    fixed URL for checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 #from purchase.views import purchase,purchase_success, cache_checkout_data,purchase_failure
 from checkout.views import checkout,payment_success,payment_failure
-from .webhooks import webhook
+from checkout.webhooks import webhook
 
 
 
