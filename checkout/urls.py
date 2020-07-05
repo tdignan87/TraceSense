fa6ee63a849f60a00589a6bdc48950c646a34972
Tom <tdignan87@gmail.com>
2020-07-05 13:48:10 +0100

    added webhooks URL

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 #from purchase.views import purchase,purchase_success, cache_checkout_data,purchase_failure
 from checkout.views import checkout,payment_success,payment_failure
+from .webhooks import webhook
 
 
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('',checkout,name="checkout"),
     path('success',payment_success, name="payment_success"),
     path('failure/',payment_failure, name="purchase_failure"),
+    path('wh/',webhook, name="webhook")
    # path('cache_checkout_data/',cache_checkout_data, name="cache_checkout_data"),
 ]
